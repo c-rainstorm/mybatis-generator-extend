9e457a8c20c25f99cdfcd65bc863360ffd764b43
baochen1.zhang <rainstorm.me@outlook.com>
2020-03-30 15:04:58 +0800

    [U] add placeholder files

---
+++ do-generate/src/main/java/me/rainstorm/HelloWorld.java
@@ -0,0 +1,13 @@
+package me.rainstorm;
+
+/**
+ * 添加这个类，是为了保留 src/main/java 目录，无实际意义
+ *
+ * @author baochen1.zhang
+ * @date 2020.03.30
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
